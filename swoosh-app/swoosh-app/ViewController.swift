9406abc11fc699e825c9604ed76e528f298aa148
Miguel Teixeira <mteixeira88013@gmail.com>
2019-06-24 16:58:23 +0100

    feat: Changing screen in iOS

--- swoosh-app/swoosh-app/ViewController.swift
+++ swoosh-app/swoosh-app/ViewController.swift
@@ -22,6 +22,10 @@
         //swoosh.frame = CGRect(x: (view.frame.size.width / 2) - (swoosh.frame.size.width / 2), y: 50 , width: swoosh.frame.size.width, height: swoosh.frame.size.height)
     //bgImg.frame = view.frame
             }
+            
+            @IBAction func unwindFromSkillVc(unwindSegue: UIStoryboardSegue) {
+                
+            }
 
 
         }
