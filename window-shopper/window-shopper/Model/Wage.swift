73281b4920b63c06401f85fd38ec58db9ae26d4e
Miguel Teixeira <mteixeira88013@gmail.com>
2019-06-25 11:53:56 +0100

    feat: first unit testing

---
+++ window-shopper/window-shopper/Model/Wage.swift
@@ -0,0 +1,15 @@
+//
+//  Wage.swift
+//  window-shopper
+//
+//  Created by Miguel Teixeira on 25/06/2019.
+//  Copyright © 2019 Miguel Teixeira. All rights reserved.
+//
+
+import Foundation
+
+class Wage {
+    class func getHours(forWage wage: Double, andPrice price: Double) -> Int {
+        return Int(ceil(price / wage))
+    }
+}
