c5f8a956666ce1d64bad1e9081051d2722d58e93
Miguel Teixeira <mteixeira88013@gmail.com>
2019-06-26 22:36:55 +0100

    feat: add struct definition

--- swoosh-app/swoosh-app/Model/Player.swift
+++ swoosh-app/swoosh-app/Model/Player.swift
@@ -9,8 +9,9 @@
 import Foundation
 
 // using struct will make the app much as faster then using a class
-// struct is used when we want to pass throughout the code and data is simple like Strings
-// struct is destroyed once is not used
+// data structure like struct's should be used always it's possilbe
+// struct data is automatically instaciated
+// struct data work like global variables, once change it will change in all the platform
 struct Player {
     var desiredLeague: String! // forcing optional unwrapping
     var selectedSkillLevel: String! // forcing optional unwrapping
